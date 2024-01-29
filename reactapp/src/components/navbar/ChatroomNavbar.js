66bb7e8cd324d9b52afb2c4537d51bf26b463f37
Ostin-Oskarose <oskarcraft@gmail.com>
2024-01-29 11:38:16 +0100

    Added login and sign up buttons

--- reactapp/src/components/navbar/ChatroomNavbar.js
+++ reactapp/src/components/navbar/ChatroomNavbar.js
@@ -1,13 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 function ChatroomNavbar() {
 
     return (
-    <Navbar className = "bg-body-tertiary" >
-        <Container fluid>
-            <Navbar.Brand>Chatroom</Navbar.Brand>
-        </Container>
+    <Navbar className="bg-body-tertiary" stickuy="top">
+            <Container fluid>
+                <Navbar.Brand>Chatroom</Navbar.Brand>
+                <Button>Login</Button>
+                <Button>Sign up</Button>
+            </Container>
     </Navbar >
     )
 }
