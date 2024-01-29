4b40e1f7a7bebbf476eb311c2837e884fa94cb5f
Ostin-Oskarose <oskarcraft@gmail.com>
2024-01-29 11:56:50 +0100

    Right aligned login and sign up buttons

--- reactapp/src/components/navbar/ChatroomNavbar.js
+++ reactapp/src/components/navbar/ChatroomNavbar.js
@@ -1,15 +1,20 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 function ChatroomNavbar() {
 
     return (
     <Navbar className="bg-body-tertiary" stickuy="top">
             <Container fluid>
-                <Navbar.Brand>Chatroom</Navbar.Brand>
-                <Button>Login</Button>
-                <Button>Sign up</Button>
+                <Col>
+                    <Navbar.Brand>Chatroom</Navbar.Brand>
+                </Col>
+                <Col className="d-flex gap-2 float-end" md="auto">
+                    <Button>Login</Button>
+                    <Button>Sign up</Button>
+                </Col>  
             </Container>
     </Navbar >
     )
