f778ba65703e057b96ef3f034ff678ba4dce55cd
Ostin-Oskarose <oskarcraft@gmail.com>
2024-11-20 20:34:35 +0100

    Added entity framework to project

---
+++ DAL/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DAL
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<User> Users;
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
